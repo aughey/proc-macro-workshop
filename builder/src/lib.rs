e0983bc50c7bd283aae840a990053637677a067e
John Aughey <jha@aughey.com>
2023-02-24 16:26:26 +0000

    progress

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,33 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let deriveinput =  parse_macro_input!(input as DeriveInput);
+
+    let ident = &deriveinput.ident;
+    let builder_name = format_ident!("{}Builder", ident);
+
+    let out = quote!(
+        pub struct #builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #ident {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    );
+    out.into()
 }
