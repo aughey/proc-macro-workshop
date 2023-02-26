550e420af2a134736c1e62b2ef03f010da275927
John Aughey <jha@aughey.com>
2023-02-26 01:59:51 +0000

    asdf

--- helper/src/lib.rs
+++ helper/src/lib.rs
@@ -1,4 +1,4 @@
-use syn::{DeriveInput, Data::Struct, Ident};
+use syn::{DeriveInput, Data::Struct, Ident, NestedMeta};
 
 pub struct DeriveInputWrapper {
     input: DeriveInput
@@ -16,10 +16,78 @@ pub struct AttributeWrapper<'a> {
     attr: &'a syn::Attribute
 }
 
-impl<'a> AttributeWrapper<'a> {
+pub struct MetaWrapper {
+    meta: syn::Meta
+}
+
+impl MetaWrapper {
+    
     pub fn key_value(&self) -> Option<syn::MetaNameValue> {
-        match &self.attr.parse_meta() {
-            Ok(syn::Meta::NameValue(nv)) => Some(nv.clone()),
+        match &self.meta {
+            syn::Meta::NameValue(nv) => Some(nv.clone()),
+            _ => None
+        }
+    }
+    pub fn key_value_string(&self) -> Option<(String,String)> {
+        let kv = self.key_value()?;
+        match &kv.lit {
+            syn::Lit::Str(s) => Some((kv.path.get_ident().unwrap().to_string(), s.value())),
+            _ => None
+        }
+    }
+}
+
+pub struct MetaListWrapper {
+    list: syn::MetaList
+}
+
+impl MetaListWrapper {
+    pub fn iter(&self) -> impl Iterator<Item = &NestedMeta> {
+        self.list.nested.iter()
+    }
+}
+
+impl AttributeWrapper<'_> {
+    pub fn like_fn(&self) -> Option<(String, (String,String))> {
+        // I'm looking for an attribute that looks like foo(bar = blech)
+        let meta = self.attr.parse_meta().ok()?;
+
+        let metalist = match meta {
+            syn::Meta::List(list) => list,
+            _ => return None
+        };
+
+        let first_ret = metalist.path.get_ident()?.to_string();
+
+        let first_nested = metalist.nested.first()?;
+
+        let name_value = match first_nested {
+            NestedMeta::Meta(syn::Meta::NameValue(nv)) => nv,
+            _ => return None
+        };
+
+        let second_ret = name_value.path.get_ident()?.to_string();
+        let third_ret = match &name_value.lit {
+            syn::Lit::Str(s) => s.value().to_string(),
+            _ => return None
+        };
+        Some((first_ret, (second_ret, third_ret)))
+    }
+}
+
+impl<'a> AttributeWrapper<'a> {
+    pub fn meta(&self) -> Option<MetaWrapper> {
+        match self.attr.parse_meta() {
+            Ok(m) => Some(MetaWrapper { meta: m }),
+            _ => None
+        }
+    }
+    pub fn get(self) -> &'a syn::Attribute {
+        self.attr
+    }
+    pub fn meta_list(&self) -> Option<MetaListWrapper> {
+        match self.attr.parse_meta() {
+            Ok(syn::Meta::List(l)) => Some( MetaListWrapper { list: l }),
             _ => None
         }
     }
@@ -33,23 +101,26 @@ impl<'a> FieldWrapper<'a> {
     pub fn ident(&self) -> Option<&Ident> {
         self.field.ident.as_ref()
     }
-    pub fn attributes(&self) -> impl Iterator<Item = AttributeWrapper> {
+    pub fn ty(&self) -> &syn::Type {
+        &self.field.ty
+    }
+    pub fn attributes(&self) -> impl Iterator<Item = AttributeWrapper<'a>> {
         self.field.attrs.iter().map(|a| AttributeWrapper { attr: a })
     }
-    pub fn attribute(&self, name: &str) -> Option<syn::Lit> {
-        if let Some(a) = self.attributes().find(|a| a.attr.path.is_ident(name)) {
-            a.key_value().map(|kv| kv.lit)
-        } else {
-            None
-        }
+
+    pub fn metas(&self) -> impl Iterator<Item = MetaWrapper> + 'a {
+        let ret = self.attributes().filter_map(|a| a.meta());
+        ret
     }
-    pub fn attribute_as_string(&self, name: &str) -> Option<String> {
-        self.attribute(name).and_then(|lit| {
-            match lit {
-                syn::Lit::Str(s) => Some(s.value()),
-                _ => None
+
+    pub fn attribute_as_string(&self, key: &str) -> Option<syn::Lit> {
+        let kvs = self.metas().filter_map(|m| m.key_value());
+        for kv in kvs {
+            if kv.path.get_ident().unwrap().to_string() == key {
+                return Some(kv.lit);
             }
-        })
+        }
+        None
     }
 }
 
@@ -73,8 +144,11 @@ impl DeriveInputWrapper {
         Self { input }
     }
 
-    pub fn add_trait_bounds(&mut self, bounds: syn::TypeParamBound) {
+    pub fn add_trait_bounds(&mut self, bounds: syn::TypeParamBound, except: &Vec<syn::Type>) {
         for param in &mut self.input.generics.type_params_mut() {
+            if except.contains(&param) {
+                continue;
+            }
             param.bounds.push(bounds.clone());
         }
     }
@@ -101,5 +175,57 @@ impl DeriveInputWrapper {
 
 #[cfg(test)]
 mod tests {
-   
+    use super::*;
+
+    fn test_struct_from_string(test_struct_str: &str) -> syn::ItemStruct {
+        let parsed_str: syn::Item = syn::parse_str(test_struct_str).unwrap();
+
+        let s = if let syn::Item::Struct(s) = parsed_str {
+            s
+        } else {
+            panic!("This should be a struct");
+        };
+
+        s   
+    }
+
+    #[test]
+    fn test_simple_struct() {
+        let test_struct_str = r#"
+        pub struct Command {
+            #[each = "arg"]
+            args: Vec<String>,
+            #[builder(each = "arg")]
+            args: Vec<String>,
+        }
+        "#;
+
+        let test_struct = test_struct_from_string(test_struct_str);
+
+        let fields = test_struct.fields.iter().map(|f| FieldWrapper { field: &f }).collect::<Vec<_>>();
+
+        assert_eq!(fields.len(), 2);
+
+        let first_field = fields.first().unwrap();
+
+        let field_attrs = first_field.attributes().collect::<Vec<_>>();
+
+        assert_eq!(field_attrs.len(), 1);
+
+        let first_attr = field_attrs.first().unwrap();
+
+        assert_eq!(first_attr.meta().unwrap().key_value_string().unwrap(),("each".to_string(),"arg".to_string()));
+
+        let second_field = fields.last().unwrap();
+
+        let field_attrs = second_field.attributes().collect::<Vec<_>>();
+
+        assert_eq!(field_attrs.len(), 1);
+
+        let first_attr = field_attrs.first().unwrap();
+
+        let likefn = first_attr.like_fn();
+
+        assert!(likefn.is_some());
+    }
 }
