189e944948ba6810b6c39e4160a39cf2da9405d1
John Aughey <jha@aughey.com>
2023-02-26 01:59:53 +0000

    dasf

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,24 +1,52 @@
 use helper::DeriveInputWrapper;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 
 
 fn do_work(input: syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream>
 {
     let mut input = DeriveInputWrapper::new(input);
 
-    input.add_trait_bounds(syn::parse_quote!(std::fmt::Debug));
+   
+    // Look for fields with PhantomData type
+    let phantom_data_types = fields.iter().filter_map(|f| {
+        if let syn::Type::Path(p) = f.ty() {
+            if let Some(segment) = p.path.segments.first() {
+                if segment.ident == "PhantomData" {
+                    // We found a PhantomData field
+                    // pull out path arguments from this path
+                        if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                            // We found angle bracketed arguments
+                            // pull out the first one
+                            if let Some(arg) = args.args.first() {
+                                if let syn::GenericArgument::Type(ty) = arg {
+                                    // We found a type argument
+                                    // return it
+                                    
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+        None
+    }).collect::<Vec<_>>();
+
+    input.add_trait_bounds(syn::parse_quote!(std::fmt::Debug), &phantom_data_types);
 
     // eschew our mutability
     let input = input;
-
-    let (impl_generics, ty_generics, where_clause) = input.generics().split_for_impl();
 
     let name = input.name();
 
     let fields = input.as_data_struct()
         .and_then(|s| s.named_fields())
         .ok_or_else(|| syn::Error::new_spanned(name, "Only structs with named fields are supported"))?;
+
+
+    let (impl_generics, ty_generics, where_clause) = input.generics().split_for_impl();
+
+
 
     let field_write = fields.iter()
         .map(|f| {
